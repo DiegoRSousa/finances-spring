5979b154b22e71920d512b4b9ee26a32c9d54bf8
Diego Sousa <diegors90@gmail.com>
2020-09-19 18:17:34 -0300

    first commit

---
+++ src/main/java/com/diego/finances/FinancesSpringApplication.java
@@ -0,0 +1,13 @@
+package com.diego.finances;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancesSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancesSpringApplication.class, args);
+	}
+
+}
